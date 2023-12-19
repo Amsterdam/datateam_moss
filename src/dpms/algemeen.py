2d17a2a26a437f08acfe5db2154d5886264cff6e
RJB <74767652+rjbreeuwer@users.noreply.github.com>
2023-12-19 13:24:04 +0100

    Update algemeen.py

--- src/dpms/algemeen.py
+++ src/dpms/algemeen.py
@@ -1,1 +1,25 @@
+def clean_column_names(cols):
+    """
+    Clean and standardize column names by converting to lowercase, replacing unwanted characters with underscores,
+    collapsing consecutive underscores, and removing leading/trailing whitespaces and underscores.
 
+    Args:
+        cols (list): List of column names to be cleaned.
+
+    Returns:
+        list: List of cleaned column names.
+
+    Example:
+        test_strings = [" First Name ", "_Last_Name_", "__Email-Address__", "Liquiditeit (current ratio)", "fte's mannen 2021?", "2017/'18*"]
+        clean_column_names(test_strings)
+
+    Usage:
+        # spark
+        df = df.toDF(*clean_column_names(df.columns))
+
+        # polars
+        df.columns = clean_column_names(df.columns)
+    """
+    UNWANTED_CHARS = r"[ ,;{}:()\n\t=\.\-/'?\*]"
+    cleaned_cols = [re.sub(UNWANTED_CHARS, '_', col.lower()).replace('___', '_').replace('__', '_').strip('_').strip() for col in cols]
+    return cleaned_cols
