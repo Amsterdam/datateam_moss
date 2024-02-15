4e155da3df609343c9f3f8adbde23d327eeaed1b
fried <f.scholvinck@amsterdam.nl>
2024-02-15 18:12:05 +0100

    bump version

--- testen_code/test.py
+++ testen_code/test.py
@@ -1,1 +1,20 @@
+# Databricks notebook source
+import unittest
+from unittest.mock import MagicMock
+from pyspark.sql import SparkSession
+from reversed_modeling import Dataset
 
+class TestDataset(unittest.TestCase):
+    def setUp(self):
+        self.spark = SparkSession.builder.getOrCreate()
+        self.dataset = Dataset(self.spark, 'catalog', 'schema', 'dataset')
+
+    def test_get_information_schema(self):
+        self.dataset.get_information_schema = MagicMock()
+        self.dataset.get_information_schema.return_value = MagicMock()
+        self.assertIsNotNone(self.dataset.get_information_schema())
+
+    # Add more test methods here for other methods in the Dataset class
+
+if __name__ == '__main__':
+    unittest.main()
