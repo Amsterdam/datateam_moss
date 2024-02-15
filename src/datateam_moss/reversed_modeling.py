275a4b66a7d916e37ea947029af29e2acb4e9661
fried <f.scholvinck@amsterdam.nl>
2024-02-15 18:05:04 +0100

    refactor whole thing

--- src/datateam_moss/reversed_modeling.py
+++ src/datateam_moss/reversed_modeling.py
@@ -15,11 +15,12 @@ class Dataset:
     Usage:
         dataset = Dataset(
             spark_session=spark, # already loaded into each databricks notebooks as 'spark'
-            catalog='catalog_name', # bijv. 'dpms_dev'
-            schema='schema_name', # bijv. 'silver'
-            project_prefixes=['prefix_'], # bijv. 'sport_'
-            exclude=['test'] 
+            catalog='catalog_name', # e.g. 'dpms_dev'
+            schema='schema_name', # e.g. 'gold'
+            dataset='dataset_name', # e.g. 'amis'
+            excluded_table_names=['amis_test1', 'amis_test2']
         )
+        dataset.create_datacontract(write_to_landingzone=True)
         dataset.print_eraser_code()
         dataset.print_drawio_code()
 
@@ -27,8 +28,8 @@ class Dataset:
         spark_session (SparkSession): The SparkSession to use, which is already loaded into each databricks notebooks as 'spark'.
         catalog (str): The name of the catalog.
         schema (str): The name of the schema.
-        project_prefix (list, optional): A list of project prefixes to filter tables. Defaults to [].
-        exclude (list, optional): A list of table names to exclude. Defaults to [].
+        dataset (str): The name of the dataset used as prefix for the tables. Defaults to '', using all tables in the schema.
+        excluded_table_names (list, optional): A list of table names to exclude. Defaults to [].
     """
 
     def __init__(self, spark_session, catalog: str, schema: str, dataset: str = '', excluded_table_names: list = []):
@@ -39,9 +40,6 @@ class Dataset:
         self.excluded_table_names = excluded_table_names
         self.information_schema = self.get_information_schema()
         self.tables_dict = self.get_tables_dict()
-        # self.occurrences = self.get_column_occurrences()
-        # self.relations = self.get_relations()
-        # self.type_of_relations = {}
 
     def get_information_schema(self):
         """
@@ -53,24 +51,12 @@ class Dataset:
         Returns:
             spark.DataFrame: A DataFrame with all columns and types in the tables.
         """
-        # df1 = spark.sql(
-        #     f"""
-        #     select c.table_name name, c.column_name attributename, c.full_data_type type , c.comment description, tg.tag_value term
-        #     from {self.catalog}.information_schema.tables t
-        #     inner join {self.catalog}.information_schema.columns c on t.table_name = c.table_name and t.table_schema = c.table_schema
-        #     left outer join  (select * from {self.catalog}.information_schema.column_tags where tag_name = 'term') tg on tg.table_name = c.table_name and tg.schema_name = c.table_schema
-
-        #     where c.table_schema = '{self.schema}'
-        #     and c.table_name LIKE '%{self.dataset}_%'
-        #     order by c.table_name,c.ordinal_position
-        #     """
-        # )
-
-        # python version of the above, first set the pyspark tables
+        # Retrieve tables, columns, and column tags from the information_schema
         tables = self.spark.table(f"{self.catalog}.information_schema.tables")
         columns = self.spark.table(f"{self.catalog}.information_schema.columns")
         column_tags = self.spark.table(f"{self.catalog}.information_schema.column_tags").filter("tag_name = 'term'")
 
+        # Join tables, columns, and column tags to get the required information
         df = (tables
             .join(columns, 
                     (col("tables.table_name") == col("columns.table_name")) & 
@@ -80,14 +66,13 @@ class Dataset:
                     (col("column_tags.table_name") == col("columns.table_name")) & 
                     (col("column_tags.schema_name") == col("columns.table_schema")), 
                     "left_outer")
-            # .filter((col("columns.table_schema") == self.schema) & col("columns.table_name").like(f"%{self.dataset}_%"))
             .filter(
                 (col("columns.table_schema") == self.schema) & 
                 col("columns.table_name").like(f"%{self.dataset}_%") & 
                 ~col("columns.table_name").isin(self.excluded_table_names)
             )
 
-            # we need to select the right columns and order them
+            # Select the required columns and order them
             .select(
                 col("columns.table_name").alias("table_name"),
                 col("columns.column_name").alias("column_name"),
@@ -116,8 +101,21 @@ class Dataset:
             'int': 'integer'
         }.get(type, 'string')
 
-    def create_datacontract(self, dataset, version='v0', datateam='MOSS', product_owner='x.name@amsterdam.nl', description='Description of the dataset', business_goal='Business goal of the dataset', write_to_landingzone=True):
-        # Convert the Spark DataFrame to pandas and prepare the tables part
+    def create_datacontract(self, version='v0', datateam='MOSS', product_owner='x.name@amsterdam.nl', description='Description of the dataset', business_goal='Business goal of the dataset', write_to_landingzone=True):
+        """
+        Creates a datacontract for the dataset.
+
+        Args:
+            version (str, optional): The version of the datacontract. Defaults to 'v0'.
+            datateam (str, optional): The name of the data team. Defaults to 'MOSS'.
+            product_owner (str, optional): The email address of the data product owner. Defaults to 'x.name@amsterdam.nl'.
+            description (str, optional): The description of the dataset. Defaults to 'Description of the dataset'.
+            business_goal (str, optional): The business goal of the dataset. Defaults to 'Business goal of the dataset'.
+            write_to_landingzone (bool, optional): Whether to write the datacontract to the landingzone. Defaults to True.
+
+        Returns:
+            str: The JSON representation of the datacontract.
+        """
         pandas_df = self.information_schema.toPandas()
         tables = [
             {
@@ -134,9 +132,8 @@ class Dataset:
             } for table_name, group in pandas_df.groupby('table_name')
         ]
 
-        # Define the data contract with the tables included
         data_contract = {
-            "Name": dataset,
+            "Name": self.dataset,
             "Version": version,
             "Description": description,
             "Business goal": business_goal,
@@ -160,7 +157,7 @@ class Dataset:
         json_data_contract = json.dumps(data_contract, indent=2)
 
         if write_to_landingzone:
-            output_path = f"/Volumes/{self.catalog}/default/landingzone/datacontracten/{dataset}.json"
+            output_path = f"/Volumes/{self.catalog}/default/landingzone/datacontracten/{self.dataset}.json"
             try:
                 with open(output_path, "w") as file:
                     file.write(json_data_contract)
@@ -180,10 +177,10 @@ class Dataset:
         Returns:
             dict: A dictionary of table names and their corresponding schemas.
         """
-        # use defaultdict to automatically handle missing keys
+        # Use defaultdict to automatically handle missing keys
         tables_dict = defaultdict(list)
 
-        # collect the DataFrame as a list of rows and directly create a dictionary
+        # Collect the DataFrame as a list of rows and directly create a dictionary
         for row in self.information_schema.collect():
             tables_dict[row['table_name']].append((row['column_name'], row['full_data_type']))
         
@@ -212,7 +209,7 @@ class Dataset:
         Returns:
             list: A list of relations between columns in different tables.
         """
-        # first get occurrences
+        # First get occurrences
         if not hasattr(self, 'occurrences'):
             self.occurrences = self._get_column_occurrences()
 
@@ -235,38 +232,28 @@ class Dataset:
                     relations.append((f'{table1}.{c}', f'{table2}.{c}', relation_type))
         return relations
 
-    # def print_eraser_code(self):
-    #     """
-    #     Prints out the full code for creating an ER Diagram in eraser.io for the dataset.
-    #     """
-    #     for table_name, schema in self.tables_dict.items():
-    #         print(table_name, '{')
-    #         for c in schema:
-    #             print('\t', c.name, c.dataType.simpleString())
-    #         print('}')
-    #         print()
 
-    #     # print relations
-    #     for c1, c2, r_type in self.relations:
-    #         print(f' {r_type} '.join((c1, c2)))
     def print_eraser_code(self, include_relations=False):
-        # Collect the DataFrame as a list of rows and directly create and print the dictionary
+        """
+        Prints out the code for creating an Eraser diagram for the dataset.
+
+        Args:
+            include_relations (bool, optional): Whether to include relations between columns in the diagram. Defaults to False.
+        """
+        # Print table definitions
         for table_name, columns in self.tables_dict.items():
             print(f"{table_name} {{")
             [print(f"    {c} {dtype}") for c, dtype in columns]
             print("}\n")
 
-
         if include_relations:
-            # print relations
+            # Print relations
             if not hasattr(self, 'relations'):
                 self.relations = self._get_relations()
             
             for c1, c2, r_type in self.relations:
                 print(f"{c1} {r_type} {c2}")
             
-
-
 
     def print_drawio_code(self):
         """
@@ -275,10 +262,8 @@ class Dataset:
         for table_name, schema in self.tables_dict.items():
             print('CREATE TABLE', table_name)
             for c in schema:
-                print(c.name)
+                print(c)
             print()
-
-
     def get_table_stats(self, table_name):
         """
         Retrieves the table statistics for a given table.
@@ -308,7 +293,7 @@ class Dataset:
         Only prints the table statistics in markdown.
         """
         if table_name == None:
-            for table_name in self.tables:
+            for table_name in self.tables_dict.keys():
                 print(table_name)
                 print(self.get_table_stats(table_name).to_markdown())
                 print()
@@ -319,30 +304,15 @@ class Dataset:
 
 # COMMAND ----------
 
-CATALOG = 'dpms_dev'
-SCHEMA = 'gold'
-DATASET = 'amis'
+# # Example usage 
+# CATALOG = 'dpms_dev'
+# SCHEMA = 'gold'
+# DATASET = 'amis'
 
-dataset = Dataset(
-    spark_session=spark,
-    catalog=CATALOG,
-    schema=SCHEMA,
-    dataset=DATASET,
-    excluded_table_names=['test']
-)
-
-# COMMAND ----------
-
-dataset.print_eraser_code()
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-r = dataset.create_datacontract('amis', write_to_landingzone=False)
-print(r)
-
-# COMMAND ----------
-
+# dataset = Dataset(
+#     spark_session=spark,
+#     catalog=CATALOG,
+#     schema=SCHEMA,
+#     dataset=DATASET,
+#     excluded_table_names=['amis_test']
+# )
