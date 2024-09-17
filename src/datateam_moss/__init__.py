5503ea32ce5e59b4b7db338373afad4a37428390
koenhallmann <144342683+koenhallmann@users.noreply.github.com>
2024-09-17 09:24:38 +0200

    Update __init__.py

--- src/datateam_moss/__init__.py
+++ src/datateam_moss/__init__.py
@@ -4,3 +4,4 @@ from datateam_moss.historisering import *
 from datateam_moss.reversed_modeling import *
 from datateam_moss.dimensioneel_modelleren import *
 from datateam_moss.label_dataset import *
+from datateam_moss.bronanalyse import *
